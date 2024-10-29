c1c62fe82f05e6e56006f3daba5247ea181aadec
Xipeng Shen <xshen5@eb2-3276-lin00.csc.ncsu.edu>
2024-10-28 23:15:10 -0400

    revised train script

--- train_LPRNet.py
+++ train_LPRNet.py
@@ -40,7 +40,7 @@ def adjust_learning_rate(optimizer, cur_epoch, base_lr, lr_schedule):
             lr = base_lr * (0.1 ** i)
             break
     if lr == 0:
-        lr = base_lr
+        lr = base_lr * 0.1
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
@@ -48,15 +48,15 @@ def adjust_learning_rate(optimizer, cur_epoch, base_lr, lr_schedule):
 
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
-    parser.add_argument('--max_epoch', default=15, help='epoch to train the network')
+    parser.add_argument('--max_epoch', default=100, help='epoch to train the network')
     parser.add_argument('--img_size', default=[94, 24], help='the image size')
-    parser.add_argument('--train_img_dirs', default="~/train", help='the train images path')
-    parser.add_argument('--test_img_dirs', default="~/test", help='the test images path')
+    parser.add_argument('--train_img_dirs', default="./data/train", help='the train images path')
+    parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
     parser.add_argument('--dropout_rate', default=0.5, help='dropout rate.')
-    parser.add_argument('--learning_rate', default=0.1, help='base value of learning rate.')
+    parser.add_argument('--learning_rate', default=0.01, help='base value of learning rate.')
     parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
-    parser.add_argument('--train_batch_size', default=128, help='training batch size.')
-    parser.add_argument('--test_batch_size', default=120, help='testing batch size.')
+    parser.add_argument('--train_batch_size', default=32, help='training batch size.')
+    parser.add_argument('--test_batch_size', default=20, help='testing batch size.')
     parser.add_argument('--phase_train', default=True, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
@@ -65,7 +65,7 @@ def get_parser():
     parser.add_argument('--test_interval', default=2000, type=int, help='interval for evaluate')
     parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
     parser.add_argument('--weight_decay', default=2e-5, type=float, help='Weight decay for SGD')
-    parser.add_argument('--lr_schedule', default=[4, 8, 12, 14, 16], help='schedule for learning rate.')
+    parser.add_argument('--lr_schedule', default=[2, 4, 6, 8, 10, 12, 14], help='schedule for learning rate.')
     parser.add_argument('--save_folder', default='./weights/', help='Location to save checkpoint models')
     # parser.add_argument('--pretrained_model', default='./weights/Final_LPRNet_model.pth', help='pretrained base model')
     parser.add_argument('--pretrained_model', default='', help='pretrained base model')
@@ -83,7 +83,7 @@ def collate_fn(batch):
         imgs.append(torch.from_numpy(img))
         labels.extend(label)
         lengths.append(length)
-    labels = np.asarray(labels).flatten().astype(np.int)
+    labels = np.asarray(labels).flatten().astype(int)
 
     return (torch.stack(imgs, 0), torch.from_numpy(labels), lengths)
 
